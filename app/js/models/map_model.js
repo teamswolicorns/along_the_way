b3bc267c7c3aca7a0fd3c443d19f5a40a8468bf0
Mandi Grant <mandi018@gmail.com>
2014-08-24 12:21:19 -0700

    Stubbed in some google map code

--- app/js/models/map_model.js
+++ app/js/models/map_model.js
@@ -14,7 +14,16 @@ var mapModel = Backbone.Model.extend({
   //puts the map in the screen
 
   initialize: function() {
-
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var start = new google.maps.LatLng(41.850033, -87.6500523); //stubbed as Chicago, need to replace with user's actual geographic location
+    var end = new google.maps.LatLng(37.76839, -122.510894); //stubbed as ocean beach, replace with user's input
+    var markers = [];
+    var mapOptions = {
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    directionsDisplay.setMap(map);
   }
 
 });
