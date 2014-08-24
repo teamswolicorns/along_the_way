7392335716a61a3b1ba552b40c0e7f26c57281cb
Mandi Grant <mandi018@gmail.com>
2014-08-24 11:58:26 -0700

    Changed attributes to origin and destination

--- app/js/map/models/map_model.js
+++ app/js/map/models/map_model.js
@@ -4,8 +4,8 @@ Backbone.$ = $;
 
 var mapModel = Backbone.Model.extend({
   defaults: {
-    locCurrent:{lat:47.60,lng:-122.33},
-    locDest:{lat:45.20,lng:-122.68},
+    origin:{lat:47.60,lng:-122.33},
+    destination:{lat:45.20,lng:-122.68},
     filters:[],
     radius:''
   },
