78d526382147728bd031f0e8d0fa751357522fd7
Mandi Grant <mandi018@gmail.com>
2014-08-24 13:02:42 -0700

    Began work on the initialize function

--- app/js/models/map_model.js
+++ app/js/models/map_model.js
@@ -2,28 +2,33 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
-var mapModel = Backbone.Model.extend({
-  url: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyAQjiLO3VzaBOgcMT2TafgLCPmiu5QySFs',
+var Map = Backbone.Model.extend({
   defaults: {
     origin:{lat:47.60,lng:-122.33},
     destination:{lat:45.20,lng:-122.68},
     filters:[],
-    radius:''
+    radius:'',
+    position:{},
+    zoom: 13,
+    maxZoom:16,
+    minZoom:10
   },
 
   //puts the map in the screen
 
-  initialize: function() {
-    directionsDisplay = new google.maps.DirectionsRenderer();
-    var start = new google.maps.LatLng(41.850033, -87.6500523); //stubbed as Chicago, need to replace with user's actual geographic location
-    var end = new google.maps.LatLng(37.76839, -122.510894); //stubbed as ocean beach, replace with user's input
-    var markers = [];
+  initialize: function(position) {
+    this.set('position',position);
+    var currentLatLng = new.google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.set('currentLatLng',currentLatLng);
     var mapOptions = {
-        zoom: 15,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+      zoom: this.get('zoom'),
+      minZoom: this.get('minZoom'),
+      maxZoom: this.get('maxZoom'),
+      center: currentLatLng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      mapTypeControl: false
     };
-    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-    directionsDisplay.setMap(map);
+    this.set('mapOptions',mapOptions);
   }
 
 });
