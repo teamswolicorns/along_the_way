49afdf15debc382ead1d21069596948674a3daf1
Mandi Grant <mandi018@gmail.com>
2014-08-24 12:17:01 -0700

    added an empty init function

--- app/js/models/map_model.js
+++ app/js/models/map_model.js
@@ -3,6 +3,7 @@ var $ = require('jquery');
 Backbone.$ = $;
 
 var mapModel = Backbone.Model.extend({
+  url: '',
   defaults: {
     origin:{lat:47.60,lng:-122.33},
     destination:{lat:45.20,lng:-122.68},
@@ -10,5 +11,10 @@ var mapModel = Backbone.Model.extend({
     radius:''
   },
 
+  //puts the map in the screen
+
+  initialize: function() {
+
+  }
 
 });
