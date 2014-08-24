99784f7af70dd08b64b4e13ee37c1b1f7962481d
Livingston Martin <livingston@drmartin.com>
2014-08-24 15:47:05 -0700

    naming convention fixed

--- app/js/client.js
+++ app/js/client.js
@@ -3,8 +3,8 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
-var MapView = require('./views/map_view');
-var MapModel = require('./models/map_model');
+var MapView = require('./views/view-map');
+var MapModel = require('./models/model-map');
 
 var map = new Map();
 var mapView = new MapView({model: map}); //makes a new map view and brings in the new map model (created above) to this new map view
