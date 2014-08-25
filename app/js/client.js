7cfd6d5e718a9a9546ddcee6853e04816c3ddac6
Mandi Grant <mandi018@gmail.com>
2014-08-24 17:53:03 -0700

    Changed module exports to Gmap to match the model, re-ordered model and view and made var map be an instance of MapModel to match the earlier require. Should fix a few more errors.

--- app/js/client.js
+++ app/js/client.js
@@ -3,10 +3,11 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
-var MapView = require('./views/view-map');
 var MapModel = require('./models/model-map');
+var MapView = require('./views/view-map');
 
-var map = new Map();
+
+var map = new MapModel({});
 var mapView = new MapView({model: map}); //makes a new map view and brings in the new map model (created above) to this new map view
 $('#map').html(mapView.$el);
 
