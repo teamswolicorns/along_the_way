1af7bc04e66e2439f6f0e0b707ab53c6d0889e08
jwarielle <jwarielle@gmail.com>
2014-08-25 15:53:01 -0700

    Modified to try to get the geolocator to set the current position as the default position. The geolocator is working, but we cannot get it to set.

--- app/js/models/model-map.js
+++ app/js/models/model-map.js
@@ -2,15 +2,44 @@ var Backbone = require('backbone');
 var $ = require('jquery');
 Backbone.$ = $;
 
-var map = Backbone.Model.extend({
+module.exports = Backbone.Model.extend({
   defaults: {
+    startLat: 0,
+    startLong: 0,
+    zoom: 8,
     mapOptions: {
+      locationArray: [],
+      startLat: 0,
+      startLong: 0,
       zoom: 8,
-      center: new google.maps.LatLng(47.601, -122.333) //needs a 3 digit decimal
+      center: new google.maps.LatLng(45.6234173, -122.3359854)
+
+       //needs a 3 digit decimal
       //might need a type?
     }
   },
-
+  initialize: function(){
+    var self = this;
+    console.log(this);
+    this.setStartLoc();
+    // this.set('mapOptions.center', new google.maps.LatLng(self.get('startLat'), self.get('startLong')));
+    this.set('mapOptions.center', new google.maps.LatLng(47.6234173, -122.3359854));
+  },
+  setStartLoc: function() {
+    if(!!navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.setLocation.bind(this));
+    } else {
+      alert("navigator.geolocation not supported");
+    }
+  },
+   setLocation: function(position) {
+      // console.log(position.coords.latitude);
+      // console.log(position.coords.longitude);
+      this.set("startLat", position.coords.latitude);
+      this.set("startLong", position.coords.longitude);
+      console.log(this.get('startLat'));
+      console.log(this.get('startLong'));
+    }
 });
 
-module.exports = map;
+
