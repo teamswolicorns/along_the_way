b36c94824008e698fdea69db4543660b7a36a5b5
Livingston Martin <livingston@drmartin.com>
2014-08-26 15:55:54 -0700

    autocomplete destination first contib

--- app/js/views/view-map.js
+++ app/js/views/view-map.js
@@ -6,17 +6,66 @@ Backbone.$ = $;
 
 module.exports = Backbone.View.extend({
   type: "Map View", //tutorial I read says this is good for debugging, not sure yet how it's used
-  id: 'content',
+  // id: 'content',
 
   initialize: function() {
     this.model.on("change mapOptions.center", this.render, this); //attach a listener
   },
 
   render: function() {
+    var template = require('../templates/template-map.hbs');
+    var data = this.model.attributes;
+    this.$el.html(template(data));
     console.log("called view-map.js render function... did a map load?");
-    var map = new google.maps.Map(this.el,this.model.get('mapOptions'));
-    map.setCenter(this.model.get('mapOptions.center'));
+    var map = new google.maps.Map(this.$('#map').get(0),this.model.get('mapOptions'));
+      map.setCenter(this.model.get('mapOptions.center'));
+
+    var input = this.$('#pac-input').get(0);
+    //   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+      autocomplete.bindTo('bounds', map);
+//////////////////Start input
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+        infowindow.close();
+        marker.setVisible(false);
+    var place = autocomplete.getPlace();
+      if (!place.geometry) {
+        return;
+      }
+          // If the place has a geometry, then present it on a map.
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      }
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);  // Why 17? Because it looks good.
+      }
+      marker.setIcon(/** @type {google.maps.Icon} */({
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+      }));
+      marker.setPosition(place.geometry.location);
+      marker.setVisible(true);
+
+      var address = '';
+      if (place.address_components) {
+        address = [
+          (place.address_components[0] && place.address_components[0].short_name || ''),
+          (place.address_components[1] && place.address_components[1].short_name || ''),
+          (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+      }
+
+      infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+      infowindow.open(map, marker);
+      /////////////////End of input
+   });
     return this; // returns everything in the map-conatiner div (google map api, new map with map options model)
   }
 });
+
 
